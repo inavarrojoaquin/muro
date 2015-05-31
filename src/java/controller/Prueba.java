f3e61edb92028a11ceda5179a61065f3a93b24f6
Joaquin Navarro <jnfebo@gmail.com>
2015-05-30 23:49:29 -0300

    Add 'prueba' file

--- src/java/controller/Prueba.java
+++ src/java/controller/Prueba.java
@@ -1,16 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import model.dao.CareerDAO;
 import model.dao.UserDAO;
+import model.dts.CareerDTO;
+import model.dts.SubjectDTO;
 import model.dts.UserDTO;
+import service.instances.CareerService;
 import service.ILoginService;
-import service.LoginService;
+import service.AbstractMuroService;
+import service.instances.LoginService;
+import service.instances.SubjectService;
 
 /**
  *
@@ -18,23 +21,40 @@ import service.LoginService;
  */
 public class Prueba {
     public static void main(String[] args){
-        UserDAO user = new UserDAO();
-        UserDTO userdto = new UserDTO("555", "555");
-        
-        ILoginService login = new LoginService();
-        UserDTO u = login.login("555", "555");
-        if(u != null){
-            System.out.println("nombre"+u.getNombre());
-            System.out.println("apellido"+u.getApellido());
-        }else {
-            System.out.println("ERROR");
-        }
-        
-        
-        
-//        LoginController login;
-//        login = new LoginController();
+//        UserDAO user = new UserDAO();
+//        UserDTO userdto = new UserDTO("555", "444");
 //        
+//        ILoginService login = new LoginService();
+//        UserDTO u = login.login("555", "444");
+//        if(u != null){
+//            System.out.println("nombre"+u.getNombre());
+//            System.out.println("apellido"+u.getApellido());
+//        }else {
+//            System.out.println("ERROR");
+//            System.out.println("User null");
+//        }
+//        
+//        AbstractMuroService career = new CareerService();
+//        List<CareerDTO> list = career.get("666");
+//        for (Iterator<CareerDTO> iterator = list.iterator(); iterator.hasNext();) {
+//            CareerDTO next = iterator.next();
+//            System.out.println("NombreCarrera: " + next.getId_carrera());
+//            System.out.println("NombreCarrera: " + next.getNombre());
+//
+//        }
         
+        AbstractMuroService subject = new SubjectService();
+        List<String> lista = new ArrayList();
+        lista.add("555");
+        lista.add("INGENIERIA INFORMATICA");
+        
+        List<SubjectDTO> listSubject = subject.get(lista);
+        for (Iterator<SubjectDTO> iterator = listSubject.iterator(); iterator.hasNext();) {
+            SubjectDTO next = iterator.next();
+            System.out.println("IdMateria: " + next.getId_materia());
+            System.out.println("NombreMateria: " + next.getNombre());
+
+        }
+               
     }
 }
